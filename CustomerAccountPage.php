97c7c2fabffb4827f42a2b6eb62b972cbf116de9
Warren Brandon Cheng <WarrenC124@gmail.com>
2023-03-10 08:11:58 -0500

    I modifed the Customer Account Page, still struggling to get it working

--- CustomerAccountPage.php
+++ CustomerAccountPage.php
@@ -1,39 +1,65 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<meta content="en-us" http-equiv="Content-Language" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<style type="text/css">
-.auto-style1 {
-	text-align: center;
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ');
+	exit;
 }
-</style>
-</head>
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'password';
+$DATABASE_NAME = 'login';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// In this case we can use the account ID to get the account info.
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
 
-<body>
 
-<p class="auto-style1"><strong>Customer Account Page </strong></p>
-<form action="CustomerAccountConnection.php" method="post">
-	Welcome, <?php echo "example_user" ?>!<br />
-	</form>
-<form action="ChatPage.html" method="post">
-<button type="button" name="button" id="chat" class="btn login_btn">Chat</button>
-    
-</form>
-
-<form action="viewAppointments.php" method="post">
-	<input name="Appointmentbtn" type="button" value="Appointments" /></form>
-<form action="" method="post">
-	<input name="EditAccountbutton" style="width: 151px" type="button" value="Edit Account" />
-</form>
-<script>
-	$(function () {
-		$('#chat').click(function (e) {
-            <?php
-    setTimeout('window.location.href = "ChatPage.html"', 1000);
-    ?>
-        }
-    });
-            </script>
-</body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>MT ART SHED</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td>Username:</td>
+						<td><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><?=$password?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$email?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
 </html>
