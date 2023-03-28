d105e470bca518d9a8f43d1debb30f1411c4f24a
Warren Brandon Cheng <WarrenC124@gmail.com>
2023-03-28 13:35:54 -0400

    Update CustomerAccountPage.php
    Here is an updated customer account page that allows the user to login, I needed to edit the config.php file will post the modified config.php file once I get everything working

--- CustomerAccountPage.php
+++ CustomerAccountPage.php
@@ -1,68 +1,100 @@
 <?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: ');
-	exit;
+
+// Check if user is logged in and name is set
+if (!isset($_SESSION['userlogin']) || !isset($_SESSION['name'])) {
+        header('Location: login.php');
+        exit();
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'password';
-$DATABASE_NAME = 'login';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
+if (isset($_SESSION['userlogin'])){
+
+$name = $_SESSION['name'];
+include "config.php";
+
+$sql = "SELECT * FROM users WHERE name = '$name'";
+
+$stmt = $db->prepare($sql);
 $stmt->execute();
-$stmt->bind_result($password, $email);
-$stmt->fetch();
-$stmt->close();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+
 ?>
-
-
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>MT ART SHED</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Profile Page</h2>
-			<div>
-				<p>Your account details are below:</p>
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><?=$_SESSION['name']?></td>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><?=$password?></td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td><?=$email?></td>
-					</tr>
-					    <td>Address:</td>
-                        <td><?=$address?></td>
-                    </tr>
-				</table>
-			</div>
-		</div>
-	</body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Profile Page</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+</head>
+
+<body>
+    <nav>
+        <div>
+            <h1>MT ART SHED</h1>
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        </div>
+    </nav>
+    <div class="content">
+    	
+    
+        <h2>Hello, <?php echo $row['name'];?></h2>
+
+        <div>
+            <p>Your account details are below:</p>
+            <table>
+                      	
+                <tr>
+                    <td>Name:</td>
+                    <td><?php echo $row['name'];?></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><?php echo $row['password'];?></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><?php echo $row['email'];?></td>
+                </tr>
+                <tr>
+                	<td>Address:</td>
+                	<td><?php echo $row['address'];?></td>
+                </tr>
+                <tr>
+                    <td>Phone Number:</td>
+                    <td><?php echo $row['phoneNumber'];?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+
+<div>
+	<button onclick= ""> Appointments</button>
+
+	
+	<button onclick= "location.href='update_account.php'"> Edit Account</button>
+</div>
+
+</body>
 </html>
+
+
+
+<?php
+
+}else{
+
+	header("location: login.php");
+ 
+ 	exit();
+}
+
+?>
