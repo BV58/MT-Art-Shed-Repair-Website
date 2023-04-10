c1116d4782e885afd7d5993710ac1dd9196a5410
Warren Brandon Cheng <51687557+theunknown124@users.noreply.github.com>
2023-04-10 09:42:09 -0400

    Add files via upload

--- CustomerAccountPage.php
+++ CustomerAccountPage.php
@@ -1,12 +1,77 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
 
+// Check if user is logged in and name is set
+if (!isset($_SESSION['userlogin'])) {
+    header('Location: login.php');
+    exit();
+}
+
+$array = $_SESSION['userlogin'];
+include "config.php";
+
+$sql = "SELECT * FROM users WHERE name = :name";
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':name', $name);
+if (!$stmt->execute()){
+    // handle the error here, e.g. log it or display an error message
+    die("Error executing query: " . $stmt->errorInfo()[2]);
+}
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+//echo var_dump($array)
+
+?>
+
+<!DOCTYPE html>
+<html>
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" x-undefined="" />
+    <meta charset="utf-8">
+    <title>Profile Page</title>
+    <link href="ProfilePage.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
-
-<body>
-
+<body class = "userlogin">
+     <nav class = "navtop">
+    <div>
+        <h1>MT ART SHED</h1>
+        <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+    </div>
+</nav>
+        <div class="content">
+            <h1>Hello, <?php echo $array['name']; ?></h1>
+        </div>
+    </nav>
+    <div class ="content">
+        <h2>Account Page</h2>
+        <div>
+            <p>Your account details are below:</p>
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td><?php echo $array['name']; ?></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><?php echo $array['email']; ?></td>
+                </tr>
+                <tr>
+                    <td>Address:</td>
+                    <td><?php echo $array['address']; ?></td>
+                </tr>
+                <tr>
+                    <td>Phone Number:</td>
+                    <td><?php echo $array['phone_number']; ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+<div>
+    <button onclick= "location.href='viewAppointments.php'"> Appointments</button>
+    <button onclick= "location.href='update_account.php'"> Edit Account</button>
+</div>
 </body>
-
 </html>
---
+++ viewAppointments.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Appointments</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wrapper {
+            width: 600px;
+            margin: 0 auto;
+        }
+
+        table tr td:last-child {
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Appointment Details</h2>
+                        <!-- <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New
+                            Appointment</a> -->
+                    </div>
+                    <?php
+                    session_start();
+                    // Include config file
+                    require_once "config.php";
+                    $email = $_SESSION['email'];
+
+                    $temp = "SELECT * FROM users WHERE email = '$email'";
+                    $result2 = $db->query($temp);
+                    $row = $result2->fetch();
+                    if ($row['authLevel'] == 1) {
+                        $sql = "SELECT * FROM appointments";
+                    } else {
+                        $sql = "SELECT * FROM appointments WHERE email = '$email'";
+                    }
+                    if ($result = $db->query($sql)) {
+                        if ($result->rowCount() > 0) {
+                            echo '<table class="table table-bordered table-striped">';
+                            echo "<thead>";
+                            echo "<tr>";
+                            echo "<th>ID Number</th>";
+                            echo "<th>Name</th>";
+                            echo "<th>Email</th>";
+                            echo "<th>Phone Number</th>";
+                            echo "<th>Address</th>";
+                            echo "<th>Date and Time</th>";
+                            echo "<th>Description</th>";
+                            echo "<th>Resolved</th>";
+                            echo "<th>Action</th>";
+                            echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                            while ($row = $result->fetch()) {
+                                echo "<tr>";
+                                echo "<td>" . $row['id'] . "</td>";
+                                echo "<td>" . $row['name'] . "</td>";
+                                echo "<td>" . $row['email'] . "</td>";
+                                echo "<td>" . $row['phoneNum'] . "</td>";
+                                echo "<td>" . $row['address'] . "</td>";
+                                echo "<td>" . $row['dateAndTime'] . "</td>";
+                                echo "<td>" . $row['description'] . "</td>";
+                                echo "<td>" . $row['resolved'] . "</td>";
+                                echo "<td>";
+                                //add action to bring to updateAccount page
+                                echo '<a href="update.php?id=' . $row['id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                echo '<a href="delete.php?id=' . $row['id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                            echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            unset($result);
+                        } else {
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+
+                    // Close connection
+                    //unset($db);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
