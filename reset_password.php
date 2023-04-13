f6100512b84b2be95632ad97088dad31b69398a9
Nigel <brendanveit@gmail.com>
2023-04-13 17:40:42 -0400

    fixed appointment

--- reset_password.php
+++ reset_password.php
@@ -37,11 +37,11 @@ if ($password != $confirm_password) {
 
 <?php
 // Hash the new password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+//$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 // Update the user's password in the database
 $stmt = $conn->prepare('UPDATE users SET password = ? WHERE email = ?');
-$stmt->bind_param('ss', $hashed_password, $email);
+$stmt->bind_param('ss', $password, $email);
 $stmt->execute();
 ?>
 
