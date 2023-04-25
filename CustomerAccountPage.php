9624a562098144e2ddaf196661dc696970632874
Brendan Veit <brendanveit@gmail.com>
2023-04-25 12:59:34 -0400

    Updated buttons on customer page

--- CustomerAccountPage.php
+++ CustomerAccountPage.php
@@ -34,6 +34,12 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
 </head>
 
 <body class="userlogin">
@@ -81,8 +87,13 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
             </table>
         </div>
     </div>
-    <div>
-        <button onclick="location.href='viewAppointments.php'"> Appointments</button>
+    <div class="text-center">
+        <a href="viewAppointments.php" class="btn btn-success pull-center"><i class="fa"></i> View or Create
+            Appointment</a>
+        <!-- <button onclick="location.href='viewAppointments.php'"> Appointments</button> -->
+        <!-- <button onclick="location.href='update_account.php'"> Edit Account</button> -->
+    </div>
+    <div class="text-center">
         <button onclick="location.href='update_account.php'"> Edit Account</button>
     </div>
 </body>
