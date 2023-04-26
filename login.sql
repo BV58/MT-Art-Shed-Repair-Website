d28ea2d84fee1c5e966a0c86096c7fc519672969
Brendan Veit <brendanveit@gmail.com>
2023-04-25 21:22:15 -0400

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 20, 2023 at 08:32 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -28,7 +19,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `appointments` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `email` varchar(319) NOT NULL,
   `phoneNum` varchar(16) NOT NULL,
@@ -39,36 +30,14 @@ CREATE TABLE `appointments` (
   `resolved` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `appointments`
---
-
-INSERT INTO `appointments` (`id`, `name`, `email`, `phoneNum`, `address`, `date`, `time`, `description`, `resolved`) VALUES
-(1, 'Jane Doe', 'janedoe@gmail.com', '8566859001', '18 PennyLane, Voorhees, New Jersey', '2023-03-31', '00:00:00', 'My shed needs repairs. The door is falling off. ', 0),
-(2, 'Brendan V', 'brendanveit@gmail.com', '8566859001', '18 Clearbrook Drive, Gibbsboro, NJ', '2023-04-20', '00:00:00', 'My shed fell apart. It needs to be rebuilt :(', 0),
-(3, 'Brendan V', 'brendanveit@gmail.com', '8566859001', '18 Clearbrook Drive, Gibbsboro, NJ', '2023-04-20', '00:00:00', 'My shed fell apart. It needs to be rebuilt :(', 0);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `email` varchar(64) NOT NULL,
   `phone_number` varchar(16) NOT NULL,
@@ -77,30 +46,11 @@ CREATE TABLE `users` (
   `authLevel` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `phone_number`, `address`, `password`, `authLevel`) VALUES
-(1, 'Jane Doe', 'janedoe@gmail.com', '278-856-4444', 'eee', 'password', 0),
-(2, 'Brendan Veit', 'brendanveit@gmail.com', '8566859001', '', 'password', 0),
-(6, 'e', 'janedoe@gmail.com', 'e', '', 'password', 0),
-(7, 'MT Art Shed Repair', 'mtartsheds@gmail.com', '856-662-7528', '27 Jones Avenue, New Brunswick, New Jersey', 'password', 1);
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `appointments`
 --
 ALTER TABLE `appointments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_resets`
---
-ALTER TABLE `password_resets`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -110,20 +60,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
---
 
 --
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `password_resets`
---
-ALTER TABLE `password_resets`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
